db49f3c28328c609e3a282e8eb5241f9055acb15
patryk <patryk@debian.patryk>
2021-01-14 23:24:26 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
+
 import random, sys
 
 alpha = '3aKbcL4dMefN5OghPiR6SjTkUlV7mWAnXoB8YpCq9ZDrsE0tFu1vGwHx2IyzJ'
 alen = len(alpha)
 tekst_we1 = "hej"
 tekst_we2 = "xu3Nq9"
-znak     = ""
-przes = 40
 indx     = 0
-code=True
-#rand_num = random.randint(10,50)
-
-#with open('encrypted','r') as f:
-#    enc_tekst = f.read().splitlines()
-
-#with open('decrypted','r') as f:
-#    dec_tekst = f.read().splitlines()
-
 
 def encrypt(tekst_we):
     rand_num = random.randint(10,50)
@@ -32,11 +21,9 @@ def encrypt(tekst_we):
     for _ in range (len(tekst_we)-1):
         tekst_wy += alpha[random.randint(0,len(alpha)-1)]
     tekst_wy += alpha[przes]
-    #print("Tekst wyjściowy to: " + tekst_wy)
     return tekst_wy
 
 def decrypt(tekst_we):
-    #rand_num = random.randint(10,50)
     custom_len = len(tekst_we)/2
     tekst_wy = ""
     temp = tekst_we[-1]
@@ -48,7 +35,6 @@ def decrypt(tekst_we):
         else:
             tekst_wy += znak
     tekst_wy = tekst_wy[:-int(custom_len)] 
-    #print("Tekst wyjściowy to: " + tekst_wy)
     return tekst_wy
 
 def full_encryption(zdanie):
@@ -64,8 +50,6 @@ def full_decryption(zdanie):
     for x in temp:
         enc_sentence += decrypt(x) + ' '
     return enc_sentence[:-1]
-
-#print(full_decryption(full_encryption('ladna pogoda dzisiaj')))
 
 ans=True
 while ans:
