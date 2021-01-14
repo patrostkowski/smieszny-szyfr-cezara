8e87df8721b2158c6d64aa866a85347129a43347
patryk <patryk@debian.patryk>
2021-01-14 23:20:14 +0100

    finale

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 # -*- coding: UTF-8 -*-
 import random, sys
 
-alfabet = '3aKbcL4dMefN5OghPiR6SjTkUlV7mWAnXoB8YpCq9ZDrsE0tFu1vGwHx2IyzJ'
-alen = len(alfabet)
+alpha = '3aKbcL4dMefN5OghPiR6SjTkUlV7mWAnXoB8YpCq9ZDrsE0tFu1vGwHx2IyzJ'
+alen = len(alpha)
 tekst_we1 = "hej"
 tekst_we2 = "xu3Nq9"
 znak     = ""
 przes = 40
 indx     = 0
 code=True
-rand_num = random.randint(10,50)
+#rand_num = random.randint(10,50)
 
 #with open('encrypted','r') as f:
 #    enc_tekst = f.read().splitlines()
@@ -20,39 +20,40 @@ rand_num = random.randint(10,50)
 
 
 def encrypt(tekst_we):
+    rand_num = random.randint(10,50)
     tekst_wy = ""
     przes = rand_num
     for znak in tekst_we:
-        indx = alfabet.find(znak)
+        indx = alpha.find(znak)
         if indx != -1 :
-            tekst_wy += alfabet[ (indx + przes) % len(alfabet) ]
+            tekst_wy += alpha[ (indx + przes) % len(alpha) ]
         else:
             tekst_wy += znak
-    for x in range (len(tekst_we)-1):
-        tekst_wy += alfabet[random.randint(0,len(alfabet)-1)]
-    tekst_wy += alfabet[przes]
-    print("Tekst wyjściowy to: " + tekst_wy)
+    for _ in range (len(tekst_we)-1):
+        tekst_wy += alpha[random.randint(0,len(alpha)-1)]
+    tekst_wy += alpha[przes]
+    #print("Tekst wyjściowy to: " + tekst_wy)
     return tekst_wy
 
 def decrypt(tekst_we):
+    #rand_num = random.randint(10,50)
     custom_len = len(tekst_we)/2
     tekst_wy = ""
     temp = tekst_we[-1]
-    przes = alfabet.find(temp)
+    przes = alpha.find(temp)
     for znak in tekst_we:
-        indx = alfabet.find(znak)
+        indx = alpha.find(znak)
         if indx != -1 :
-            tekst_wy += alfabet[ (indx - przes) % len(alfabet) ]
+            tekst_wy += alpha[ (indx - przes) % len(alpha) ]
         else:
             tekst_wy += znak
     tekst_wy = tekst_wy[:-int(custom_len)] 
-    print("Tekst wyjściowy to: " + tekst_wy)
+    #print("Tekst wyjściowy to: " + tekst_wy)
     return tekst_wy
 
 def full_encryption(zdanie):
     enc_sentence = ''
     temp = zdanie.split()
-    print(temp)
     for x in temp:
         enc_sentence += encrypt(x) + ' '
     return enc_sentence[:-1]
@@ -60,12 +61,32 @@ def full_encryption(zdanie):
 def full_decryption(zdanie):
     enc_sentence = ''
     temp = zdanie.split()
-    print(temp)
     for x in temp:
         enc_sentence += decrypt(x) + ' '
     return enc_sentence[:-1]
 
 #print(full_decryption(full_encryption('ladna pogoda dzisiaj')))
 
-
+ans=True
+while ans:
+    print ("""
+    1.Zakoduj wiadmosc.
+    2.Odkoduj wiadmosc.
+    3.Exit.\n
+    """)
+    ans=input("Co chcialbys zrobic?:") 
+    if ans=="1": 
+        value = input('Wpisz wiadmosc do zakodowania: ')
+        print(f'Zakodowana wiadomosc to: {full_encryption(value)}')
+        print('\n')
+    elif ans=="2":
+        value = input('Wpisz wiadomosc do odkodowania: ')
+        print(f'Zakodowana wiadomosc to: {full_decryption(value)}')
+        print('\n')
+    elif ans=="3":
+        print("\n Do nastepnego!")
+        ans = None
+        sys.exit()
+    elif ans !="":
+        print("\n Blad!") 
 
